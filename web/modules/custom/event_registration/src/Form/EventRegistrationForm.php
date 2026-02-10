fcd6319caa23c29f77caf507fd360694f06eb431
sumaiya0786 <sumaiyabth2018@gmail.com>
2026-02-10 20:44:33 +0530

    Initialize EventRegistrationForm class

--- web/modules/custom/event_registration/src/Form/EventRegistrationForm.php
+++ web/modules/custom/event_registration/src/Form/EventRegistrationForm.php
@@ -1,1 +1,16 @@
+<?php
+
+namespace Drupal\event_registration\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class EventRegistrationForm extends FormBase {
+
+  public function getFormId() {
+    return 'event_registration_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
 
